f5633377dcbb2dcf910142b91fb53e3595bf9720
Kent McKinney <kent.p.mckinney@gmail.com>
2020-06-02 13:15:50 -0700

    add SVG icon files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
+import { MemoryRouter, Switch, Route, Link } from 'react-router-dom';
 import Header from './Header';
 import Footer from './Footer';
 import './App.scss';
 import ingredientList from './test.json';
+import searchIcon from './search.svg';
+import menuIcon from './menu.svg';
+import totalIcon from './total.svg';
 
 
 class App extends React.Component {
-  state = { view: {} }
+  constructor(props) {
+    super(props);
+    this.ingredients = <div>{ingredientList.map(i => <div className='ingredient' key={i[1]} id={i[1]}>{i[0]}</div>)}</div>
+    this.state = { view: {} };
+  }
 
   onOrientationChange = () => {
     const isPortrait = window.matchMedia("(orientation: portrait)").matches;
@@ -14,6 +22,7 @@ class App extends React.Component {
   }
 
   onScreenWidthChange = () => {
+    console.log('screen width change')
     const isLargeScreenWidth = window.matchMedia("(min-width: 768px) and (orientation: portrait)").matches ||
       window.matchMedia("(min-width: 1024px) and (orientation: landscape)").matches;
     const isLargeScreenHeight = window.matchMedia("(min-height: 800px) and (orientation: portrait)").matches ||
@@ -35,14 +44,23 @@ class App extends React.Component {
     this.setState({ view: { ...this.state.view, isLargeScreen } });
   }
 
+  onChangeTheme = () => {
+    const isLightTheme = window.matchMedia("(prefers-color-scheme: light)");
+    this.setState({ view: { ...this.state.view, isLightTheme } });
+  }
+
   setMediaQueryListeners = () => {
-    window.matchMedia("(orientation: portrait)").addListener(e => this.onScreenOrientationChange());
-    window.matchMedia("(width)").addListener(e => this.onScreenWidthChange());
-    window.matchMedia("(height)").addListener(e => this.onScreenHeightChange());
+    window.matchMedia("(prefers-color-scheme: light)").addEventListener('change', this.onChangeTheme);
+    window.matchMedia("(orientation: portrait)").addEventListener('change', this.onScreenOrientationChange);
+    window.matchMedia("(min-width: 768px)").addEventListener('change', this.onScreenWidthChange);
+    window.matchMedia("(min-width: 1024px)").addEventListener('change', this.onScreenWidthChange);
+    window.matchMedia("(min-height: 600px)").addEventListener('change', this.onScreenHeightChange);
+    window.matchMedia("(min-height: 800px)").addEventListener('change', this.onScreenHeightChange);
   }
 
   setInitialMediaQueryState = () => {
     const isHandheld = window.matchMedia("(handheld)").matches;
+    const isLightTheme = window.matchMedia("(prefers-color-scheme: light)");
     const isPortrait = window.matchMedia("(orientation: portrait)").matches;
     const isLargeScreenWidth = window.matchMedia("(min-width: 768px) and (orientation: portrait)").matches ||
       window.matchMedia("(min-width: 1024px) and (orientation: landscape)").matches;
@@ -63,32 +81,66 @@ class App extends React.Component {
 
   }
 
-  renderHandheldPortrait = () => {
+  viewHandheldPortrait = () => {
+
+    const footer = <Footer links={[
+      { name: 'Search', img: { searchIcon }, to: '/' },
+      { name: 'Menu', img: { menuIcon }, to: '/menu' },
+      { name: 'Totals', img: { totalIcon }, to: '/totals' }
+    ]}></Footer>
+
     return (
       <div className="grid">
-        <div className="header"><Header /></div>
-        <div className="content-container">
-          <div className="content">
-            {ingredientList.map(i => <div className='ingredient' key={i[1]} id={i[1]}>{i[0]}</div>)}
-          </div>
-        </div>
-        <div className="footer"><Footer /></div>
+        <MemoryRouter>
+          <Switch>
+            <Route path='/' exact>
+              <div className="header">
+                <Header />
+              </div>
+              <div className="content-container">
+                <div className="content">
+                  {this.ingredients}
+                </div>
+              </div>
+              <div className="footer">{footer}</div>
+            </Route>
+            <Route path='/menu' exact>
+              <div className="header"><Header /></div>
+              <div className="content-container">
+                <div className="content">
+                  Menu
+                </div>
+              </div>
+              <div className="footer">{footer}</div>
+            </Route>
+            <Route path='/totals' exact>
+              <div className="header"><Header /></div>
+              <div className="content-container">
+                <div className="content">
+                  Totals
+                </div>
+              </div>
+              <div className="footer">{footer}</div>
+            </Route>
+          </Switch>
+        </MemoryRouter>
       </div>
     );
   }
 
-  renderHandheldLandscape = () => {
+  viewHandheldLandscape = () => {
 
   }
 
-  render = () => {
+  showContent = () => {
     console.log(this.state.view)
     const { isHandheld, isPortrait, isLargeScreen } = this.state.view;
-    const content = this.renderHandheldPortrait();
+    const content = this.viewHandheldPortrait();
+    return content;
+  }
 
-    return (
-      <div className="app">{content}</div>
-    );
+  render = () => {
+    return <div className="app">{this.showContent()}</div>;
   }
 
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Footer.scss';
 
 const Footer = props => {
+
+  console.log(props.links[0].img)
   return (
     <div className="footer">
       <div className="footer-grid">
-        <div className="content-left"><span role='img' aria-label='Search'>🔍</span></div>
-        <div className="content-center"><span role='img' aria-label='Menu'>✎</span></div>
-        <div className="content-right"><span role='img' aria-label='Totals'>∑</span></div>
+        <div className="content-left"><span role='img' aria-label='Search'><Link to={props.links[0].to}><img src={props.links[0].img.searchIcon} /></Link></span></div>
+        <div className="content-center"><span role='img' aria-label='Menu'><Link to={props.links[1].to}><img src={props.links[1].img.menuIcon} /></Link></span></div>
+        <div className="content-right"><span role='img' aria-label='Totals'><Link to={props.links[2].to}><img src={props.links[2].img.totalIcon} /></Link></span></div>
       </div>
     </div>
   )
